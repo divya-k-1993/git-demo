65c575006d8b37a44d01c45b7132587352d49973
divya-k-1993 <divyakushwah2026@gmail.com>
2026-02-09 16:53:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     print(f"10 + 5 = {add(10, 5)}")
     print(f"10 - 5 = {subtract(10, 15)}")
     print(f"10 * 5 = {multiply(10, 5)}")
-    print(f"10 / 5 = {divide(10, 5)}")
+    print(f"10 / 5 = {divide(5, 5)}")
